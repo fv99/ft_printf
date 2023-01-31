9be2314d2b669ba3f9616de0b78aeae12303d5c1
fvonsovs <filipvon@protonmail.com>
2023-01-31 17:59:09 +0100

    Added main function

--- ft_printf.c
+++ ft_printf.c
@@ -1,16 +1,36 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_printf.c                                        :+:      :+:    :+:   */
+/*   ft_ft_printf.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: fvonsovs <fvonsovs@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/27 12:42:46 by fvonsovs          #+#    #+#             */
-/*   Updated: 2023/01/27 12:50:37 by fvonsovs         ###   ########.fr       */
+/*   Updated: 2023/01/31 16:40:47 by fvonsovs         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "libftprintf.h"
+#include "libftft_printf.h"
+
+
+
+int	print(char **out, int *varg)
+{
+	register int	len;
+	register int	pad;
+	register int	i;
+	register char	*format;
+	char			scr[2];
+
+	i = 0;
+	len = 0;
+	pad = 0;
+	format = (char *)(*varg++);
+
+
+
+}
+
 
 int	ft_printf(const char *input, ...)
 {
@@ -18,4 +38,38 @@ int	ft_printf(const char *input, ...)
 
 
 
+}
+
+int main(void)
+{
+	char *ptr ="Hello world!";
+	char *np = 0;
+	it i = 5;
+	unsigned int bs = sizeof(int)*8;
+	int mi;
+	char buf[80];
+
+	mi = (1 << (bs-1)) + 1;
+	ft_printf("%s\n", ptr);
+	ft_printf("ft_printf test\n");
+	ft_printf("%s is null pointer\n", np);
+	ft_printf("%d = 5\n", i);
+	ft_printf("%d = - max int\n", mi);
+	ft_printf("char %c = 'a'\n", 'a');
+	ft_printf("hex %x = ff\n", 0xff);
+	ft_printf("hex %02x = 00\n", 0);
+	ft_printf("signed %d = unsigned %u = hex %x\n", -3, -3, -3);
+	ft_printf("%d %s(s)%", 0, "message");
+	ft_printf("\n");
+	ft_printf("%d %s(s) with %%\n", 0, "message");
+	ft_printf(buf, "justif: \"%-10s\"\n", "left"); ft_printf("%s", buf);
+	ft_printf(uf, "justif: \"%10s\"\n", "right"); ft_printf("%s", buf);
+	ft_printf(buf, " 3: %04d zero padded\n", 3); ft_printf("%s", buf);
+	ft_printf(buf, " 3: %-4d left justif.\n", 3); ft_printf("%s", buf);
+	ft_printf(buf, " 3: %4d right justif.\n", 3); ft_printf("%s", buf);
+	ft_printf(buf, "-3: %04d zero padded\n", -3); ft_printf("%s", buf);
+	ft_printf(buf, "-3: %-4d left justif.\n", -3); ft_printf("%s", buf);
+	ft_printf(buf, "-3: %4d right justif.\n", -3); ft_printf("%s, buf);
+
+	eurn 0;
 }
--- libftprintf.h
+++ libftprintf.h
@@ -6,7 +6,7 @@
 /*   By: fvonsovs <fvonsovs@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/27 12:41:22 by fvonsovs          #+#    #+#             */
-/*   Updated: 2023/01/27 12:50:45 by fvonsovs         ###   ########.fr       */
+/*   Updated: 2023/01/31 17:58:56 by fvonsovs         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,18 @@
 # include <stdlib.h>
 # include <unistd.h>
 # include "libft.a"
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 255
+# endif
+
+# ifndef PAD_RIGHT
+#  define PAD_RIGHT 1
+# endif
+
+# ifndef PAD_ZERO
+#  define PAD_ZERO 2
+# endif
 
 int	ft_printf(const char *input, ...);
 
