d26cb3d6316baa7a582fc7548c311be7bcaba29e
practical <filipvon@protonmail.com>
2023-02-06 15:49:13 +0100

    Added function to ft_printf (unfinished)

--- ft_printf.c
+++ ft_printf.c
@@ -10,41 +10,105 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "libftft_printf.h"
+#include "ft_printf.h"
 
-
-
-int	print(char **out, int *varg)
+static int	prints(int *varg, char *format, int width, int pad)
 {
-	register int	len;
-	register int	pad;
-	register int	i;
-	register char	*format;
-	char			scr[2];
+	int	printed;
+	int	padchar;
 
-	i = 0;
-	len = 0;
-	pad = 0;
-	format = (char *)(*varg++);
+	printed = 0;
+	padchar = ' ';
+
+	if (width > 0)
+	{
+		
+
+
+	}
+
+}
+
+static int	printi()
+{
+
+
+}
+
+static int	ft_print(int *varg, char *format, int printed, int width, int pad)
+{
+	char *s;
+
+	if (*format == 's')
+	{
+		s = *((char **)varg++);
+		
+
+	}
 
 
 
 }
 
+static int	ft_format(int *varg, char *format, int printed)
+{
+	int		width;
+	int		pad;
+
+	while (*format != 0)
+	{
+		++format;
+		width = pad = 0;
+		if (*format == '-')
+		{
+			++format;
+			pad = PAD_RIGHT;
+		}
+		while (*format == '0')
+		{
+			++format;
+			pad |= PAD_ZERO;
+		}
+		while (*format >= '0' && *format <= '9')
+		{
+			++format;
+			width *= 10;
+			width += *format - '0';
+		}
+		ft_print(*varg, *format, printed, width, pad);
+
+
+	}
+}
+
+
 
 int	ft_printf(const char *input, ...)
 {
+	int		*varg;
+	char	*format;
+	int		printed;
 
-
-
-
+	varg = (int *)(&input);
+	format = (char *)(*varg++);
+	printed = 0;
+	while (*format != 0)
+	{
+		format++;
+		if (*format == '%')
+			ft_format(*varg++, *format, printed);
+		else
+			ft_putchar_fd(*format, 1);
+		printed++;
+	}
+	return (printed);
 }
 
 int main(void)
 {
 	char *ptr ="Hello world!";
 	char *np = 0;
-	it i = 5;
+	int i = 5;
 	unsigned int bs = sizeof(int)*8;
 	int mi;
 	char buf[80];
@@ -63,13 +127,13 @@ int main(void)
 	ft_printf("\n");
 	ft_printf("%d %s(s) with %%\n", 0, "message");
 	ft_printf(buf, "justif: \"%-10s\"\n", "left"); ft_printf("%s", buf);
-	ft_printf(uf, "justif: \"%10s\"\n", "right"); ft_printf("%s", buf);
+	ft_printf(buf, "justif: \"%10s\"\n", "right"); ft_printf("%s", buf);
 	ft_printf(buf, " 3: %04d zero padded\n", 3); ft_printf("%s", buf);
 	ft_printf(buf, " 3: %-4d left justif.\n", 3); ft_printf("%s", buf);
 	ft_printf(buf, " 3: %4d right justif.\n", 3); ft_printf("%s", buf);
 	ft_printf(buf, "-3: %04d zero padded\n", -3); ft_printf("%s", buf);
 	ft_printf(buf, "-3: %-4d left justif.\n", -3); ft_printf("%s", buf);
-	ft_printf(buf, "-3: %4d right justif.\n", -3); ft_printf("%s, buf);
+	ft_printf(buf, "-3: %4d right justif.\n", -3); ft_printf("%s", buf);
 
-	eurn 0;
+	return (0);
 }
